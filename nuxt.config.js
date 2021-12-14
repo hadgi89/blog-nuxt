19959e96b6ba6a595caa50f5fa8dd4c2537c93b4
zim <zim@localhost>
2021-12-14 22:37:24 +0200

    Что такое модули. установка axios

--- nuxt.config.js
+++ nuxt.config.js
@@ -36,7 +36,9 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
   ],
+  axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
