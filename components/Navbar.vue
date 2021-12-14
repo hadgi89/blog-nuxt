69af1f1b4ae985e40790a4d7143c012611c5afef
zim <zim@localhost>
2021-12-14 22:13:16 +0200

    Ссылки и обработка ошибок

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -7,13 +7,13 @@
         <div class="collapse navbar-collapse">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <nuxt-link class="nav-link active" aria-current="page" to="/">Home</nuxt-link>
+              <nuxt-link exact active-class="active" class="nav-link" to="/">Home</nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/users">Users</nuxt-link>
+              <nuxt-link no-prefetch active-class="active" class="nav-link" to="/users">Users</nuxt-link>
             </li>
             <li class="nav-item">
-              <nuxt-link class="nav-link" to="/about">About</nuxt-link>
+              <nuxt-link no-prefetch active-class="active" class="nav-link" to="/about">About</nuxt-link>
             </li>
           </ul>
         </div>
