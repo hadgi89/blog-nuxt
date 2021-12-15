ddc5f8241a1b35d56a00fc688e159451a7125997
zim <zim@localhost>
2021-12-15 14:45:01 +0200

    Работа с Vuex

--- pages/users/index.vue
+++ pages/users/index.vue
@@ -12,16 +12,13 @@
 
 <script>
 export default {
-  asyncData({$axios, error}) {
-    return $axios.$get('https://jsonplaceholder.typicode.com/users')
-      .then(users => {
-        return {
-          users
-        }
-      })
-      .catch(err => {
-        error(err)
-      })
+  async asyncData({store, error}) {
+    try {
+      const users = await store.dispatch('users/fetchUsers')
+      return {users}
+    } catch (e) {
+      error(e)
+    }
   },
   data() {
     return {
