ce1edc8af2362f1bbcc71f604ecedcc7af7ff769
zim <zim@localhost>
2021-12-15 15:57:03 +0200

    Middleware и защита роуто

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,5 +1,16 @@
 <template>
   <section>
     <h1>About Page</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem eveniet aliquid cum sequi quod quia natus ipsum reiciendis exercitationem aut.</p>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi, sapiente. Perferendis modi deserunt, pariatur exercitationem alias adipisci. Repellendus in perspiciatis, libero, debitis, ad quis asperiores ratione saepe voluptate earum corporis.</p>
   </section>
 </template>
+
+
+<script>
+export default {
+  middleware: ['auth']
+}
+</script>
